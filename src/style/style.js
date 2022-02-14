c606a4fb486b3337aa9cac108e4b78f8321ed040
Robert <u1501851@rts-365.dk>
2022-02-14 11:34:21 +0100

    Created syle.js with style variables

---
+++ src/style/style.js
@@ -0,0 +1,32 @@
+export const colors = {
+    grey: '#EAEAEA ',
+    purple: '#5E2E53',
+    pink: '#E1A1E9',
+    pinkTrans: 'rgba(225, 161, 233, 0.8)',
+    black: '#000000',
+    white: '#FFF'
+}
+
+export const fontSize = {
+    s: '18px',
+    m: '24px',
+    l: '36px'
+}
+
+export const spacing = {
+    s: '.5rem',
+    m: '1rem',
+    l: '1.5rem',
+    xl: '2rem',
+    xxl: '2.5rem'
+}
+
+export const shadow = {
+    one: '3px 4px 4px rgba(0, 0, 0, 0.25)'
+}
+
+export const borderRadius = {
+    s: '10px',
+    m: '11px',
+    l: '39px'
+}
