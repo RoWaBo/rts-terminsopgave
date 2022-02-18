9512ce93cdedbeadfc176a9e604c3d6e45d8dcb5
Robert <u1501851@rts-365.dk>
2022-02-18 12:57:46 +0100

    Reduced code with activitiesDependingOnRole, linkDependingOnRole in Calendar

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -13,28 +13,24 @@ import axios from "axios";
 const Calendar = () => {
     const navigate = useNavigate();
     const { auth, user } = useContext(UserContext)
-    const [activities, setActivities] = useState();
-    const [instructorTeams, setInstructorTeams] = useState();
+    const [instructorTeams, setInstructorTeams] = useState([]);
 
     useEffect(() => {
         !auth.token && navigate('/logind')
     }, [auth.token, navigate]);
 
-    // Set activities if user role is instrutor
+    // Get activities and set instructorTeams if user id match the activities instructorId
     useEffect(() => {
-        if (activities || user?.role !== 'instructor') return
+        if (user?.role !== 'instructor') return
         (async () => {
             const { data: activities } = await axios('http://localhost:4000/api/v1/activities')
-            setActivities(activities)
+            const filteredActivities = activities.filter(activity => user.id === activity.instructorId)
+            setInstructorTeams([...filteredActivities])
         })()
-    }, [activities, user]);
+    }, [user]);
 
-    // Set instructorTeams if user id match the activities instructorId
-    useEffect(() => {
-        if (!activities) return
-        const filteredActivities = activities.filter(activity => user.id === activity.instructorId)
-        setInstructorTeams([...filteredActivities])
-    }, [activities, user]);
+    const activitiesDependingOnRole = () => user.role === 'instructor' ? instructorTeams : user.activities
+    const linkDependingOnRole = (activityID) => user.role === 'instructor' ? `/holdoversigt/${activityID}` : `/aktivitetsdetaljer/${activityID}`
 
     // === STYLE ===
     const listItemStyle = css`
@@ -45,16 +41,9 @@ const Calendar = () => {
         <MainLayout>
             <PageHeader heading="kalender" isFixed />
             <List>
-                {user?.role === 'instructor' && instructorTeams?.map((team) => (
-                    <li key={team.id} css={listItemStyle}>
-                        <Link to={`/holdoversigt/${team.id}`}>
-                            <ActivityCardSmall activityInfo={team} />
-                        </Link>
-                    </li>
-                ))}
-                {user?.role === 'default' && user?.activities.map((activity) => (
+                {user && activitiesDependingOnRole().map((activity) => (
                     <li key={activity.id} css={listItemStyle}>
-                        <Link to={`/aktivitetsdetaljer/${activity.id}`}>
+                        <Link to={linkDependingOnRole(activity.id)}>
                             <ActivityCardSmall activityInfo={activity} />
                         </Link>
                     </li>
