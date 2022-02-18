49e80152463b773aa1d90f6b16330be56e0efe90
Robert <u1501851@rts-365.dk>
2022-02-18 23:26:18 +0100

    SearchBar animation

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,35 +2,57 @@ import { FiSearch } from 'react-icons/fi'
 import { css } from "@emotion/react";
 /** @jsxImportSource @emotion/react */
 import { colors, fontSize, spacing } from "../style/style";
+import { motion } from 'framer-motion';
 
 const SearchInput = ({ inputValue, inputOnChangeFcn }) => {
 
     const inputStyle = css`
-    width: 100%;
-    background: transparent;
-    border: none;
-    outline: none;
-    font-family: 'Ubuntu', sans-serif;
-    font-weight: normal;
-    font-size: ${fontSize.s};
-    color: ${colors.white};
+        width: 100%;
+        background: transparent;
+        border: none;
+        outline: none;
+        font-family: 'Ubuntu', sans-serif;
+        font-weight: normal;
+        font-size: ${fontSize.s};
+        color: ${colors.white};
     `
     const searchStyle = css`
-    display: flex;
-    place-items: center;
-    height: 50px;
-    background-color: ${colors.greyTrans};
-    padding: 0 12px;
-    margin: 10px ${spacing.gutter}; 
+        display: flex;
+        place-items: center;
+        height: 50px;
+        background-color: ${colors.greyTrans};
+        padding: 0 12px;
+        margin: 10px ${spacing.gutter};
     `
     const searchIcon = css`
-    width: 24px;
-    height: 24px;
-    color: ${colors.grey};
+        width: 24px;
+        height: 24px;
+        min-width: 24px;
+        min-height: 24px;
+        color: ${colors.grey};
     `
 
+    // === ANIMATIONS ===
+    const containerMotion = {
+        initial: {
+            opacity: 0,
+            width: '12%'
+        },
+        animate: {
+            opacity: 1,
+            width: '86%',
+            transition: {
+                duration: .7
+            }
+        }
+    }
+
     return (
-        <div css={searchStyle}>
+        <motion.div css={searchStyle}
+            initial='initial'
+            animate='animate'
+            variants={containerMotion}
+        >
             <input
                 type="text"
                 css={inputStyle}
@@ -38,7 +60,7 @@ const SearchInput = ({ inputValue, inputOnChangeFcn }) => {
                 onChange={(e) => inputOnChangeFcn(e.target.value)}
             />
             <FiSearch css={searchIcon} />
-        </div>
+        </motion.div>
     );
 }
 
