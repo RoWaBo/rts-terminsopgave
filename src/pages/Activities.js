f61d6b49d17b11fb62639367cd58f7558de2e7cf
Robert <u1501851@rts-365.dk>
2022-03-01 12:32:05 +0100

    Test

--- src/pages/Activities.js
+++ src/pages/Activities.js
@@ -10,54 +10,54 @@ import MainLayout from "../components/MainLayout";
 import { motion } from "framer-motion";
 
 const Activities = () => {
+	const [activities, setActivities] = useState();
 
-    const [activities, setActivities] = useState();
+	useEffect(() => {
+		if (activities) return;
+		(async () => {
+			const { data: activities } = await axios(
+				"http://localhost:4000/api/v1/activities"
+			);
+			setActivities(activities);
+		})();
+	}, [activities]);
 
-    useEffect(() => {
-        if (activities) return
-        (async () => {
-            const { data: activities } = await axios('http://localhost:4000/api/v1/activities')
-            setActivities(activities)
-        })()
-    }, [activities]);
+	// === STYLE ===
+	const listStyle = css`
+		padding: 0 ${spacing.gutter};
+		/* account for fixed header */
+		margin-top: 110px;
 
-    // === STYLE ===
-    const listStyle = css`
-        padding: 0 ${spacing.gutter};
-        /* account for fixed header */
-        margin-top: 110px;
+		& > * {
+			margin-bottom: 31px;
+		}
+	`;
 
-        & > * {
-            margin-bottom: 31px;
-        }
-    `
-
-    return (
-        <MainLayout>
-            <PageHeader heading="aktiviteter" isFixed />
-            <ul css={listStyle}>
-                {activities?.map((activity) => (
-                    <motion.li
-                        key={activity.id}
-                        initial={{ opacity: 0, x: '-100vw' }}
-                        animate={{ opacity: 1, x: 0 }}
-                        exit={{ opacity: 0 }}
-                        transition={{
-                            duration: .5,
-                            delay: `0.${activity.id}5`,
-                            type: 'spring',
-                            stiffness: 170,
-                            damping: 15
-                        }}
-                    >
-                        <Link to={`/aktivitetsdetaljer/${activity.id}`}>
-                            <ActivityCard activityInfo={activity} />
-                        </Link>
-                    </motion.li>
-                ))}
-            </ul>
-        </MainLayout>
-    );
-}
+	return (
+		<MainLayout>
+			<PageHeader heading="aktiviteter test" isFixed />
+			<ul css={listStyle}>
+				{activities?.map(activity => (
+					<motion.li
+						key={activity.id}
+						initial={{ opacity: 0, x: "-100vw" }}
+						animate={{ opacity: 1, x: 0 }}
+						exit={{ opacity: 0 }}
+						transition={{
+							duration: 0.5,
+							delay: `0.${activity.id}5`,
+							type: "spring",
+							stiffness: 170,
+							damping: 15,
+						}}>
+						<Link to={`/aktivitetsdetaljer/${activity.id}`}>
+							<ActivityCard activityInfo={activity} />
+						</Link>
+					</motion.li>
+				))}
+			</ul>
+		</MainLayout>
+	);
+};
 
 export default Activities;
